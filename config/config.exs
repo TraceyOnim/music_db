c2decab02f087ff218615614835164a32d5bc358
Tee22 <traceyOnim22@gmail.com>
2020-01-06 05:57:16 +0300

    Added custom count function

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,8 @@ import Config
 
 config :music_db, MusicDb.Repo,
   database: "music_db_repo",
-  username: "user",
-  password: "pass",
+  username: "postgres",
+  password: "postgres",
   hostname: "localhost"
 
 config :music_db, ecto_repos: [MusicDb.Repo]
--- lib/music_db/repo.ex
+++ lib/music_db/repo.ex
@@ -2,4 +2,8 @@ defmodule MusicDb.Repo do
   use Ecto.Repo,
     otp_app: :music_db,
     adapter: Ecto.Adapters.Postgres
+
+  def count(table) do
+    aggregate(table, :count)
+  end
 end
